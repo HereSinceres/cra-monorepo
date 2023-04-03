ec01e71dee2f600a0e68c28b49e40a1c30a2f4f3
xukai12 <xukai12@baidu.com>
2023-04-03 11:41:53 +0800

    12
    Change-Id: Ib46088fe89e4a06e5c8db387ba5ccfb1727c7472

--- apps/spa-app/src/App.tsx
+++ apps/spa-app/src/App.tsx
@@ -1,29 +1,17 @@
 import './App.css';
 
+import {wordToNum} from 'lib-webpack/lib/util/aa/sum2'
 import React from 'react';
-import { Button } from 'ui/Button';
+import { Button } from 'ui/lib/Button';
 
 import c from './index.module.scss';
 import logo from './logo.svg';
 
+alert(wordToNum('one'))
 function App() {
   return (
     <div className={  `App ${c.red}`}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className={c.red}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button text="Test Button"/>
-      </header>
+      <Button text="Test Button"/>
     </div>
   );
 }
--- apps/spa-app/src/index.tsx
+++ apps/spa-app/src/index.tsx
@@ -1,22 +1,22 @@
 import './index.css';
 
-import {numToWord}  from 'lib-webpack/util/aa/sum2';
+import { numToWord } from 'lib-webpack/lib/util/aa/sum2';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Button } from 'ui/Button';
+import { Button } from 'ui/lib/Button';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 console.log(numToWord(1));
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-    <Button text="Texts" />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+        <Button text='Texts' />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
